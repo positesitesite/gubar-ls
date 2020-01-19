77defaf0542391f057829af8923e9bcdd4a30572
Arthur <arthur.gubar.x@gmail.com>
2020-01-19 14:50:13 +0700

    Section ' intro, about ' were modified

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 ## if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/positesitesite/positesitesite.github.io master:gh-pages
+git push -f https://github.com/positesitesite/positesitesite.github.io master:master
 
 ## if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f https://<USERNAME>.github.io/<REPO> master:gh-pages
