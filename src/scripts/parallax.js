e6d50d47aaa9c3761c1a6143ff0c1b7fe301ccf0
Arthur <arthur.gubar.x@gmail.com>
2020-01-26 22:00:59 +0700

    Vue installed

---
+++ src/scripts/parallax.js
@@ -0,0 +1,17 @@
+const sky = document.querySelector('.sky');
+const mountains = document.querySelector('.mountains');
+const train = document.querySelector('.train');
+const grass = document.querySelector('.grass');
+
+window.onscroll = () => {
+  let yOffset = window.pageYOffset;
+
+  if (yOffset < 870) {
+    sky.style.transform = `translate3d(0, ${-(yOffset / 100) + '%'}, 0)`;
+    mountains.style.transform = `translate3d(0, ${-(yOffset / 75) + '%'}, 0)`;
+    train.style.transform = `translate3d(0, ${-(yOffset / 50) + '%'}, 0)`;
+    grass.style.transform = `translate3d(0, ${-(yOffset / 25) + '%'}, 0)`;
+  }
+
+  console.log(yOffset);
+}
